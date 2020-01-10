4d475dd8eaae9a4775e35d3d811d34a2ae327243
tomiyee <tommy.and.heng@gmail.com>
2020-01-10 08:40:12 -0500

    Init the new Project

---
+++ projects/2048/main.js
@@ -0,0 +1,18 @@
+
+
+
+
+window.onload = start;
+
+function start () {
+
+  // Add The event listeners
+  window.addEventListener("keydown", keyDownHandler);
+}
+
+function keyDownHandler (e) {
+  console.log(e.keyCode);
+  switch (e.keyCode) {
+
+  }
+}
