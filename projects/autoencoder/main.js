315cb6cf1ce387f6c49fd5de0df4aceeb7868a49
tomiyee <tommy.and.heng@gmail.com>
2020-01-29 12:41:52 -0500

    Added Downscaling and Cloning Input

--- projects/autoencoder/main.js
+++ projects/autoencoder/main.js
@@ -4,8 +4,12 @@ const INPUT_HEIGHT = 28;
 const PIXEL_SCALE = 6;
 const DRAWING_SCALE = 8;
 const ANIM_WIDTH = 400;
-const BRUSH_RADIUS = min(INPUT_WIDTH, INPUT_HEIGHT)*PIXEL_SCALE / 8;
+const BRUSH_RADIUS = min(INPUT_WIDTH, INPUT_HEIGHT)*PIXEL_SCALE / 10;
 
+// All of the Autoencoders involved
+let autoencoder32, encoder32, decoder32;
+let autoencoder16, encoder16, decoder16;
+let autoencoder8, encoder8, decoder8;
 // All of the Canvases involved;
 let input, input32, input16, input8;
 let inputCtx, input32Ctx, input16Ctx, input8Ctx;
@@ -16,17 +20,21 @@ let shrunk, shrunkCtx;
 
 $(start);
 function start () {
+  // Loads the Tensorflowjs models
   initModels ();
-
+  // Initializes the Canvases
   initCanvases ();
-
-
 }
 
 function initModels () {
 
 }
 
+/**
+ * @function initCanvases - Initializes the look of each of the canvases and
+ * any event handlers tied to the canvases, such as the dragging handler for the
+ * input
+ */
 function initCanvases () {
   // 1. Initializes the look of every canvas
 
@@ -42,38 +50,44 @@ function initCanvases () {
     canvas8.width = INPUT_WIDTH*PIXEL_SCALE;
     canvas8.height = INPUT_HEIGHT*PIXEL_SCALE;
     canvas8Ctx = canvas8.getContext('2d');
-    canvas8Ctx.fillStyle = 'black'
+    canvas8Ctx.imageSmoothingEnabled = false;
+    canvas8Ctx.fillStyle = 'black';
     canvas8Ctx.fillRect(0, 0, canvas8.width, canvas8.height);
   canvas16 = document.getElementById('output-canvas-16');
     canvas16.width = INPUT_WIDTH*PIXEL_SCALE;
     canvas16.height = INPUT_HEIGHT*PIXEL_SCALE;
     canvas16Ctx = canvas16.getContext('2d');
-    canvas16Ctx.fillStyle = 'black'
+    canvas16Ctx.imageSmoothingEnabled = false;
+    canvas16Ctx.fillStyle = 'black';
     canvas16Ctx.fillRect(0, 0, canvas16.width, canvas16.height);
   canvas32 = document.getElementById('output-canvas-32');
     canvas32.width = INPUT_WIDTH*PIXEL_SCALE;
     canvas32.height = INPUT_HEIGHT*PIXEL_SCALE;
     canvas32Ctx = canvas32.getContext('2d');
-    canvas32Ctx.fillStyle = 'black'
+    canvas32Ctx.imageSmoothingEnabled = false;
+    canvas32Ctx.fillStyle = 'black';
     canvas32Ctx.fillRect(0, 0, canvas32.width, canvas32.height);
   // 1c. The Downscaled Input Canvases
   input8  = document.getElementById('input-canvas-8' );
     input8.width = INPUT_WIDTH*PIXEL_SCALE;
     input8.height = INPUT_HEIGHT*PIXEL_SCALE;
     input8Ctx = input8.getContext('2d');
-    input8Ctx.fillStyle = 'black'
+    input8Ctx.imageSmoothingEnabled = false;
+    input8Ctx.fillStyle = 'black';
     input8Ctx.fillRect(0, 0, input8.width, input8.height);
   input16 = document.getElementById('input-canvas-16');
     input16.width = INPUT_WIDTH*PIXEL_SCALE;
     input16.height = INPUT_HEIGHT*PIXEL_SCALE;
     input16Ctx = input16.getContext('2d');
-    input16Ctx.fillStyle = 'black'
+    input16Ctx.imageSmoothingEnabled = false;
+    input16Ctx.fillStyle = 'black';
     input16Ctx.fillRect(0, 0, input16.width, input16.height);
   input32 = document.getElementById('input-canvas-32');
     input32.width = INPUT_WIDTH*PIXEL_SCALE;
     input32.height = INPUT_HEIGHT*PIXEL_SCALE;
     input32Ctx = input32.getContext('2d');
-    input32Ctx.fillStyle = 'black'
+    input32Ctx.imageSmoothingEnabled = false;
+    input32Ctx.fillStyle = 'black';
     input32Ctx.fillRect(0, 0, input32.width, input32.height);
   // 1d. The animation canvases in between
   animation8  = document.getElementById('animation-canvas-8');
@@ -90,26 +104,40 @@ function initCanvases () {
     shrunk.width = INPUT_WIDTH;
     shrunk.height = INPUT_HEIGHT;
     shrunkCtx = shrunk.getContext('2d');
-
   // 2. Activate the Event Handlers
-  $(input).bind('mousedown', (e) => {
-    input.dragging = true;
-  }).bind ('mousemove', (e) => {
-    if (!input.dragging)
-      return;
-    const coords = relativeCoords(e, input);
-    brush(coords.x, coords.y);
-  }).bind ('mouseup', (e) => {
-    input.dragging = false;
-  });
-
+  $(input)
+    .bind('mousedown', (e) => input.dragging = true)
+    .bind ('mousemove', dragHandler)
+    .bind ('mouseup', (e) => input.dragging = false);
 }
 
+/**
+ * @function dragHandler - Callback for dragging on the input canvas. This handles
+ * drawing on the canvas and shrinking this canvas.
+ *
+ * @param  {MouseEvent} e Mouse Event Data
+ */
+function dragHandler (e) {
+  if (!input.dragging)
+    return;
+  const coords = relativeCoords(e, input);
+  brush(coords.x, coords.y);
+  shrink();
+}
+
+/**
+ * @function brush - Draws a gradient circle onto the input canvas at the given
+ * x and y coordinates. The brush starts at 75% transparent white and fades to
+ * become more transparent radially outwards.
+ *
+ * @param  {Number} x The center x coord of the circle for the brush
+ * @param  {Number} y The center y coord of the circle for the brush
+ */
 function brush (x, y) {
   var grd = inputCtx.createRadialGradient(x, y, 0, x, y, BRUSH_RADIUS);
   grd.addColorStop(0, rgba(255,255,255,0.25));
+  grd.addColorStop(0.5, rgba(255,255,255,0.1));
   grd.addColorStop(1, rgba(255,255,255,0));
-
   // Fill with gradient
   inputCtx.fillStyle = grd;
   inputCtx.beginPath();
@@ -117,12 +145,19 @@ function brush (x, y) {
   inputCtx.fill();
 }
 
+function shrink () {
+  shrunkCtx.drawImage(input, 0, 0, shrunk.width, shrunk.height);
+  input32Ctx.drawImage(shrunk, 0, 0, input32.width, input32.height);
+  input16Ctx.drawImage(shrunk, 0, 0, input16.width, input16.height);
+  input8Ctx.drawImage(shrunk, 0, 0, input8.width, input8.height);
+}
+
 /**
  * Returns an obect with the x and y coordinates of the mouse event to the given html element
  *
- * @param  {MouseEvent} event   description
- * @param  {HTML Element} element description
- * @return {Object}         description
+ * @param  {MouseEvent}   event   The Mouse Event data
+ * @param  {HTML Element} element The element we want the coordinates relative to
+ * @return {Object}       The x and y coordinates relative to the top left of the given element
  */
 function relativeCoords (event, element) {
     var x = event.clientX - element.getBoundingClientRect().left;
